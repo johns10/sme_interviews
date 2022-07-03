2d11a7ae9561edaa8770dc38048512b5536e776c
John Davenport <johns10@gmail.com>
2022-07-03 16:16:32 -0400

    r

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :sme_interviews, SmeInterviewsWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   load_from_system_env: true,
   http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   server: true, # Without this line, your app will not start the web server!
@@ -16,10 +17,6 @@ config :sme_interviews, SmeInterviews.Repo,
   database: "",
   ssl: true,
   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
-
-config :sme_interviews, SmeInterviewsWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
 
