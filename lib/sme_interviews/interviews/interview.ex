22acf1bad1f0213ab62255a8a03cdb4114a20da7
John Davenport <johns10@gmail.com>
2022-07-04 01:52:49 -0400

    prod

--- lib/sme_interviews/interviews/interview.ex
+++ lib/sme_interviews/interviews/interview.ex
@@ -2,10 +2,13 @@ defmodule SmeInterviews.Interviews.Interview do
   use Ecto.Schema
   import Ecto.Changeset
   alias SmeInterviews.Questions.Question
+  alias SmeInterviews.Accounts.User
 
   schema "interviews" do
     field :description, :string
     field :name, :string
+
+    belongs_to :user, User
 
     has_many :questions, Question
 
--- priv/repo/migrations/20220630040630_create_interviews.exs
+++ priv/repo/migrations/20220630040630_create_interviews.exs
@@ -5,8 +5,11 @@ defmodule SmeInterviews.Repo.Migrations.CreateInterviews do
     create table(:interviews) do
       add :name, :string
       add :description, :string
+      add :user_id, references("users", on_delete: :nilify_all)
 
       timestamps()
     end
+
+    create index(:interviews, [:user_id])
   end
 end
