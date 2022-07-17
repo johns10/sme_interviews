e4b09532b9c3aa7a5ff252767278945b2f2185cc
John Davenport <johns10@gmail.com>
2022-07-17 16:06:19 -0400

    put zoomsdk on window

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,3 +66,7 @@ liveSocket.connect();
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket;
+
+import zoomSdk from "@zoom/appssdk"
+
+window.zoomSdk = zoomSdk
