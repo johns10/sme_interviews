c8585ef8a603a374e58afc4e372cc795e8e96dad
John Davenport <johns10@gmail.com>
2022-09-03 09:52:51 -0400

    Just use id, no magic here

--- assets/js/lib/models.js
+++ assets/js/lib/models.js
@@ -1,7 +1,7 @@
 const MODEL_NAME = "models"
 
-function createModel(db, { blob, version }) {
-  return db.create({ type: MODEL_NAME, blob, id: version })
+function createModel(db, { blob, id }) {
+  return db.create({ type: MODEL_NAME, blob, id })
 }
 
 function getModel(db, version) {
@@ -16,4 +16,40 @@ function clearModels(db) {
   return db.clear({ type: MODEL_NAME })
 }
 
-export { createModel, getModel, clearModels, listModels }
+async function ensureModel(db, version, url) {
+  result = await getModel(db, version)
+  if (result) return result
+  else {
+    const blob = await fetchFile(url)
+    await createModel(db, { blob, version })
+    return blob
+  }
+}
+
+export { createModel, getModel, clearModels, listModels, ensureModel }
+
+function fetchFile(url) {
+  return new Promise(
+    (resolve, reject) => {
+      fetch(url)
+        .then(response => {
+          const reader = response.body.getReader()
+          const chunks = []
+          reader.read().then(function storeChunk({ done, value }) {
+            if (done) {
+              const length = chunks.reduce((acc, chunk) => acc + chunk.length, 0)
+              const { result: fileArray } = chunks.reduce(({ offset, result }, chunk) => {
+                const newOffset = offset + chunk.length;
+                result.set(chunk, offset)
+                return { offset: newOffset, result: result }
+              }, { offset: 0, result: new Uint8Array(length) })
+              resolve(new Blob([fileArray]));
+            } else {
+              if (value) chunks.push(value)
+              return reader.read().then(storeChunk)
+            }
+          })
+        })
+    }
+  )
+}
--- assets/js/test/model.test.js
+++ assets/js/test/model.test.js
@@ -7,23 +7,23 @@ const database = new Database()
 beforeAll(async () => { return database.initialize() })
 
 test('put model works', async () => {
-  const attrs = { blob: "test", version: 1 }
+  const attrs = { blob: "test", id: 1 }
   await Models.createModel(database, attrs)
-  const result = await Models.getModel(database, attrs.version)
+  const result = await Models.getModel(database, attrs.id)
   const { version, ...fields } = attrs
-  expect(result).toStrictEqual(fields);
+  expect(result).toStrictEqual({ blob: attrs.blob });
 })
 
 test('list models works', async () => {
-  const attrs = { blob: "test", version: 1 }
+  const attrs = { blob: "test", id: 1 }
   await Models.createModel(database, attrs)
   const result = await Models.listModels(database)
   const { version, ...fields } = attrs
-  expect(result).toStrictEqual([fields])
+  expect(result).toStrictEqual([{ blob: attrs.blob }])
 })
 
 test('clear model works', async () => {
-  const attrs = { blob: "test", version: 1 }
+  const attrs = { blob: "test", id: 1 }
   await Models.createModel(database, attrs)
   await Models.clearModels(database)
   const result = await Models.listModels(database, attrs.version)
